d22f505679eaf3b7475b340c1822dde006059b98
mishyTheBear <hamish.dickson@gmail.com>
2014-07-07 19:04:39 +0100

    Change server stuff based on 'DEV', 'TEST' and 'LIVE' (blank is LIVE'

--- app/config.js
+++ app/config.js
@@ -2,6 +2,7 @@
 // HWD
 //
 
+
 //
 // jobs5
 //
@@ -12,11 +13,21 @@ exports.PORT = 3000;
 //
 exports.jobs_rest_port = 8070;
 
-// this is my mac's ip
-exports.jobs_rest_host = "192.168.1.5";
-
-// this is my work pc's ip (no doubt this won't work in windows)
-//exports.jobs_rest_host = "172.24.24.217";
-
 // jira
 exports.jira_rest_host = "jira.jhc.co.uk";
+
+// default to live server
+exports.jobs_rest_host = "172.24.24.217";
+
+exports.LEVEL = 'DEV';
+/*
+ * overrides level
+ */
+
+if (exports.LEVEL == 'DEV') {
+    exports.jobs_rest_host = "192.168.1.5";
+} else if (exports.LEVEL == 'TEST') {
+
+} else {
+
+}
--- app/server.js
+++ app/server.js
@@ -8,28 +8,37 @@ var config = require('./config.js');
 
 var page_hdlr = require('./handlers/pages.js');
 var helpers = require('./handlers/helpers.js');
-var jobs_hdlr = require('./handlers/jobs.js');
-var persons_hdlr = require('./handlers/persons.js');
-var jira_hdlr = require('./handlers/jira.js');
+var jobs_hdlr, persons_hdlr, jira_hdlr;
 
 app.use(express.logger('dev'));
 app.use(express.bodyParser({ keepExtensions: true }));
 app.use(express.static(__dirname + "/../static"));
 
+// this will always be like this
 app.get('/pages/:page_name', page_hdlr.generate);
 app.get('/pages/:page_name/:sub_page', page_hdlr.generate);
 
-app.get('/jobs/test/post', jobs_hdlr.post_job);
+// TODO remove later
+if (config.LEVEL == 'DEV' || config.LEVEL == 'TEST') {
+    jobs_hdlr = require('./handlers/test/jobs.js');
+    persons_hdlr = require('./handlers/test/persons.js');
+    jira_hdlr = require('./handlers/test/jira.js');
+} else {
+    jobs_hdlr = require('./handlers/jobs.js');
+    persons_hdlr = require('./handlers/persons.js');
+    jira_hdlr = require('./handlers/jira.js');
+}
 
-app.get('/jobs/test', jobs_hdlr.get_test_jobs);
+//app.get('/jobs/test/post', jobs_hdlr.post_job);
+
 app.get('/jobs/:jobNumber', jobs_hdlr.get_specific_job);
 app.get('/jobs/user/:user', jobs_hdlr.get_users_jobs);
 app.get('/jobs/user/:user/:status', jobs_hdlr.get_users_jobs_for_status);
 
-app.get('/persons/test', persons_hdlr.get_test_persons);
+//app.get('/persons/test', persons_hdlr.get_test_persons);
 app.get('/persons/:person', persons_hdlr.get_persons);
 
-app.get('/jira/test', jira_hdlr.get_test_jira);
+//app.get('/jira/test', jira_hdlr.get_test_jira);
 app.get('/jira/user/:user', jira_hdlr.get_users_jira);
 
 app.get("/", function (req, res) {
