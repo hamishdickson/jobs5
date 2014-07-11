6d4ad838d1cafbd70358fa9c53a948c7dfdbd7bb
mishyTheBear <hamish.dickson@gmail.com>
2014-07-11 08:48:24 +0100

    Small UI changes

--- static/content/homeJobsGetter.js
+++ static/content/homeJobsGetter.js
@@ -89,7 +89,7 @@ $(document).ready(function () {
             });
 
             // get the jobs3 data
-            $.getJSON("/jobs/test" + jobNumber, function (d) {
+            $.getJSON("/jobs/" + jobNumber, function (d) {
                 $.extend(tdataSpecific, d);
             });
 
