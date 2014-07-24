22a5470ed03417aa7b25bda90fe9f35d69b81861
mishyTheBear <hamish.dickson@gmail.com>
2014-07-24 09:02:10 +0100

    mmm  COOOOOOOOKIES

--- static/content/home.js
+++ static/content/home.js
@@ -3,6 +3,14 @@
 //
 
 $(function() {
+    var url = window.location;
+    // Will only work if string in href matches with location
+    $('ul.nav a[href="'+ url +'"]').parent().addClass('active');
+
+    // Will also work for relative and absolute hrefs
+    $('ul.nav a').filter(function() {
+        return this.href == url;
+    }).parent().addClass('active');
 
 	var tmpl, // main template HTML
         tdata = {}; // JSON data object that feeds the template
--- static/content/settings.js
+++ static/content/settings.js
@@ -2,22 +2,86 @@
  * Created by hamishdickson on 23/07/2014.
  */
 
-$(function() {
+$(function () {
+
+    // change which menu option is active
+    var url = window.location;
+    $('ul.nav a[href="' + url + '"]').parent().addClass('active');
+
+    $('ul.nav a').filter(function () {
+        return this.href == url;
+    }).parent().addClass('active');
+
     var tmpl, tdata = {};
 
-    var url = window.location.pathname.split("/");
+    // Get the job number from the url
+    var urlArray = window.location.pathname.split("/");
+    var jobNumber = urlArray[3];
 
-    // this doesn't feel that safe ...
-    var jobNumber = url[3];
+    // must specify that we the cookie is json
+    $.cookie.json = true;
 
-    var initPage = function() {
-        $.get("/templates/settings.html", function(d) {
+    var value = $.cookie('Jobs5');
+
+    // if there isn't a cookie, create one
+    if (value === undefined) {
+        var cookieData = {
+            "name": "",
+            "rememberMe": false,
+            "helpText": false,
+            "removeMe": false
+        };
+
+        $.cookie('Jobs5', cookieData);
+    } else {
+    }
+
+    var initPage = function () {
+        $.get("/templates/settings.html", function (d) {
             tmpl = d;
         });
 
         $(document).ajaxStop(function (d) {
-            var renderedPage = Mustache.to_html( tmpl, tdata );
-            $("#data").html( renderedPage );
+            var renderedPage = Mustache.to_html(tmpl, tdata);
+            $("#data").html(renderedPage);
+
+            // add in the cookie data
+            $("span.user").text(value.name);
+
+            if (value.rememberMe) {
+                $("#remember-me-onoff").attr('checked', true);
+            }
+
+            if (value.helpText) {
+                $("#help-text-onoff").attr('checked', true);
+            }
+
+            $('#remember-me-onoff').click(function () {
+                value.rememberMe = this.checked;
+
+                $("#remove-me-onoff").attr('checked', false);
+
+                $.cookie('Jobs5', value);
+            });
+
+            $('#help-text-onoff').click(function () {
+                value.helpText = this.checked;
+
+                $("#remove-me-onoff").attr('checked', false);
+
+                $.cookie('Jobs5', value);
+            });
+
+            $('#remove-me-onoff').click(function () {
+                if (this.checked) {
+                    $("#remember-me-onoff").attr('checked', false);
+                    $("#help-text-onoff").attr('checked', false);
+
+                    // delete cookie
+                    $.removeCookie('Jobs5');
+                }
+            });
+
         });
     }();
 });
