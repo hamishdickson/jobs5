c10c484cd1cf89ee8c643997330ac7ca572eaa76
mishyTheBear <hamish.dickson@gmail.com>
2014-07-26 16:35:50 +0100

    Get server working

--- server.js
+++ server.js
@@ -6,14 +6,20 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+app.set('port', process.env.PORT || 3070);
+
 app.use(express.logger('dev'));
 app.use(express.bodyParser({ keepExtensions: true }));
-app.use(express.static(__dirname + "/../static"));
+app.use(express.static(path.join(__dirname, 'public'), { maxAge: 86400000 }));
+
+console.log("Starting up server - port 3071");
+
+app.listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+});
+
 
 app.get("*", function (req, res) {
     res.redirect('/#' + req.originalUrl);
     res.end();
 });
-
-console.log("Starting up server - port 3071");
-app.listen(3071);
