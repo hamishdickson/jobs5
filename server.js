87da905de7e05c73e9b8578db297327af817afc6
mishyTheBear <hamish.dickson@gmail.com>
2014-07-26 18:30:31 +0100

    Refactor to use angularjs part 2

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.logger('dev'));
 app.use(express.bodyParser({ keepExtensions: true }));
 app.use(express.static(path.join(__dirname, 'public'), { maxAge: 86400000 }));
 
-console.log("Starting up server - port 3071");
+console.log("Starting up server - port 3070");
 
 app.listen(app.get('port'), function() {
     console.log('Express server listening on port ' + app.get('port'));
