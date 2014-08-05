c81e51d7348d60d5f99dfded7a2af90d624a0ebb
mishyTheBear <hamish.dickson@gmail.com>
2014-08-05 21:32:40 +0100

    Refactor number a lot

--- public/controllers/job.js
+++ public/controllers/job.js
@@ -15,6 +15,7 @@ var app = angular.module('MyApp');
         this.addNote = function(job) {
             this.note.createdOn = Date.now();
             job.notes.push(this.note);
+            job.response = this.note.response;
             this.note = {};
         };
     });
@@ -22,36 +23,36 @@ var app = angular.module('MyApp');
 var testJobs = [
     {"jobNumber": 123456, "description": "first description 123456", "whoDo": "HD", "status": "A", "client": "JHC", "importance": 3, "whoPay": "JHC", "contact": "Hamish", "workorder": 2, "jobType": "J", "enteredBy": "HD", "functionalArea": "WEBWEB", "system": "TRACEY", "invoiceText": "Test Job 1", "enteredDate": 20140624, "enteredTime": 900, "defect": "N", "liveUat": "L", "releaseVersion": "F63", "project": "JOBS", "urgent": "Y", "deliverables": [
         {"jobNumber": 123456, "id": 1, "promisedDate": 20140901, "type": "I", "description": "First deliverable", "deliveredDate": 0, "whoDo": "HD", "deleted": "N", "app": "F63", "internal": "Y"},
-    ], "notes": []
+    ], "notes": [], "response":""
     },
     {"jobNumber": 223456, "description": "2 description", "whoDo": "HD", "status": "A", "client": "JHC", "importance": 3, "whoPay": "JHC", "contact": "Hamish", "workorder": 2, "jobType": "J", "enteredBy": "HD", "functionalArea": "WEBWEB", "system": "TRACEY", "invoiceText": "Test Job 1", "enteredDate": 20140624, "enteredTime": 900, "defect": "N", "liveUat": "L", "releaseVersion": "F63", "project": "JOBS", "urgent": "Y", "deliverables": [
         {"jobNumber": 123456, "id": 1, "promisedDate": 20140723, "type": "I", "description": "First deliverable", "deliveredDate": 0, "whoDo": "HD", "deleted": "N", "app": "F63", "internal": "Y"},
         {"jobNumber": 123456, "id": 1, "promisedDate": 20141001, "type": "I", "description": "First deliverable", "deliveredDate": 0, "whoDo": "HD", "deleted": "N", "app": "F63", "internal": "Y"}
-    ], "notes": []},
+    ], "notes": [], "response":""},
     {"jobNumber": 323456, "description": "3 description", "whoDo": "HD", "status": "W", "client": "JHC", "importance": 3, "whoPay": "JHC", "contact": "Hamish", "workorder": 2, "jobType": "J", "enteredBy": "HD", "functionalArea": "WEBWEB", "system": "TRACEY", "invoiceText": "Test Job 1", "enteredDate": 20140624, "enteredTime": 900, "defect": "N", "liveUat": "L", "releaseVersion": "F63", "project": "JOBS", "urgent": "Y", "deliverables": [
         {"jobNumber": 123456, "id": 1, "promisedDate": 20140723, "type": "I", "description": "First deliverable", "deliveredDate": 0, "whoDo": "HD", "deleted": "N", "app": "F63", "internal": "Y"},
         {"jobNumber": 123456, "id": 1, "promisedDate": 20141001, "type": "I", "description": "First deliverable", "deliveredDate": 0, "whoDo": "HD", "deleted": "N", "app": "F63", "internal": "Y"}
-    ], "notes": []},
+    ], "notes": [], "response":""},
     {"jobNumber": 423456, "description": "4 description", "whoDo": "HD", "status": "A", "client": "JHC", "importance": 3, "whoPay": "JHC", "contact": "Hamish", "workorder": 2, "jobType": "J", "enteredBy": "HD", "functionalArea": "WEBWEB", "system": "TRACEY", "invoiceText": "Test Job 1", "enteredDate": 20140624, "enteredTime": 900, "defect": "N", "liveUat": "L", "releaseVersion": "F63", "project": "JOBS", "urgent": "Y", "deliverables": [
         {"jobNumber": 123456, "id": 1, "promisedDate": 20140701, "type": "I", "description": "First deliverable", "deliveredDate": 0, "whoDo": "HD", "deleted": "N", "app": "F63", "internal": "Y"},
         {"jobNumber": 123456, "id": 1, "promisedDate": 20140501, "type": "I", "description": "First deliverable", "deliveredDate": 0, "whoDo": "HD", "deleted": "N", "app": "F63", "internal": "Y"}
-    ], "notes": []},
+    ], "notes": [], "response":""},
     {"jobNumber": 523456, "description": "5 description", "whoDo": "HD", "status": "A", "client": "JHC", "importance": 3, "whoPay": "JHC", "contact": "Hamish", "workorder": 2, "jobType": "J", "enteredBy": "HD", "functionalArea": "WEBWEB", "system": "TRACEY", "invoiceText": "Test Job 1", "enteredDate": 20140624, "enteredTime": 900, "defect": "N", "liveUat": "L", "releaseVersion": "F63", "project": "JOBS", "urgent": "Y", "deliverables": [
         {"jobNumber": 123456, "id": 1, "promisedDate": 20140701, "type": "I", "description": "First deliverable", "deliveredDate": 0, "whoDo": "HD", "deleted": "N", "app": "F63", "internal": "Y"},
         {"jobNumber": 123456, "id": 1, "promisedDate": 20140501, "type": "I", "description": "First deliverable", "deliveredDate": 0, "whoDo": "HD", "deleted": "N", "app": "F63", "internal": "Y"}
-    ], "notes": []},
+    ], "notes": [], "response":""},
     {"jobNumber": 623456, "description": "6 description", "whoDo": "HD", "status": "A", "client": "JHC", "importance": 3, "whoPay": "JHC", "contact": "Hamish", "workorder": 2, "jobType": "J", "enteredBy": "HD", "functionalArea": "WEBWEB", "system": "TRACEY", "invoiceText": "Test Job 1", "enteredDate": 20140624, "enteredTime": 900, "defect": "N", "liveUat": "L", "releaseVersion": "F63", "project": "JOBS", "urgent": "Y", "deliverables": [
         {"jobNumber": 123456, "id": 1, "promisedDate": 20140701, "type": "I", "description": "First deliverable", "deliveredDate": 0, "whoDo": "HD", "deleted": "N", "app": "F63", "internal": "Y"},
         {"jobNumber": 123456, "id": 1, "promisedDate": 20140501, "type": "I", "description": "First deliverable", "deliveredDate": 0, "whoDo": "HD", "deleted": "N", "app": "F63", "internal": "Y"}
-    ], "notes": []},
+    ], "notes": [], "response":""},
     {"jobNumber": 723456, "description": "7 description", "whoDo": "HD", "status": "A", "client": "JHC", "importance": 3, "whoPay": "JHC", "contact": "Hamish", "workorder": 2, "jobType": "J", "enteredBy": "HD", "functionalArea": "WEBWEB", "system": "TRACEY", "invoiceText": "Test Job 1", "enteredDate": 20140624, "enteredTime": 900, "defect": "N", "liveUat": "L", "releaseVersion": "F63", "project": "JOBS", "urgent": "Y", "deliverables": [
         {"jobNumber": 123456, "id": 1, "promisedDate": 20140701, "type": "I", "description": "First deliverable", "deliveredDate": 0, "whoDo": "HD", "deleted": "N", "app": "F63", "internal": "Y"},
         {"jobNumber": 123456, "id": 1, "promisedDate": 20140501, "type": "I", "description": "First deliverable", "deliveredDate": 0, "whoDo": "HD", "deleted": "N", "app": "F63", "internal": "Y"}
-    ], "notes": []},
+    ], "notes": [], "response":""},
     {"jobNumber": 823456, "description": "8 description", "whoDo": "HD", "status": "A", "client": "JHC", "importance": 3, "whoPay": "JHC", "contact": "Hamish", "workorder": 2, "jobType": "J", "enteredBy": "HD", "functionalArea": "WEBWEB", "system": "TRACEY", "invoiceText": "Test Job 1", "enteredDate": 20140624, "enteredTime": 900, "defect": "N", "liveUat": "L", "releaseVersion": "F63", "project": "JOBS", "urgent": "Y", "deliverables": [
         {"jobNumber": 123456, "id": 1, "promisedDate": 20140901, "type": "I", "description": "First deliverable", "deliveredDate": 0, "whoDo": "HD", "deleted": "N", "app": "F63", "internal": "Y"}
-    ], "notes": []},
+    ], "notes": [], "response":""},
     {"jobNumber": 923456, "description": "9 description", "whoDo": "HD", "status": "A", "client": "JHC", "importance": 3, "whoPay": "JHC", "contact": "Hamish", "workorder": 2, "jobType": "J", "enteredBy": "HD", "functionalArea": "WEBWEB", "system": "TRACEY", "invoiceText": "Test Job 1", "enteredDate": 20140624, "enteredTime": 900, "defect": "N", "liveUat": "L", "releaseVersion": "F63", "project": "JOBS", "urgent": "Y", "deliverables": [
         {"jobNumber": 123456, "id": 1, "promisedDate": 20140901, "type": "I", "description": "First deliverable", "deliveredDate": 0, "whoDo": "HD", "deleted": "N", "app": "F63", "internal": "Y"}
-    ], "notes": []}
+    ], "notes": [], "response":""}
 ];
