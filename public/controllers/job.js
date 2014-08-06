2432cd5a579157ccdb7a5cce02ce8456d87e783b
mishyTheBear <hamish.dickson@gmail.com>
2014-08-06 08:41:58 +0100

    Remove unneeded controller files

--- public/controllers/job.js
+++ public/controllers/job.js
@@ -20,6 +20,19 @@ var app = angular.module('MyApp');
         };
     });
 
+    app.controller('TabsController', function () {
+        this.tab = 1;
+
+        this.setTab = function(newValue) {
+            this.tab = newValue;
+        };
+
+        this.isSet = function(tabName) {
+            return this.tab === tabName;
+        }
+
+    });
+
 var testJobs = [
     {"jobNumber": 123456, "description": "first description 123456", "whoDo": "HD", "status": "A", "client": "JHC", "importance": 3, "whoPay": "JHC", "contact": "Hamish", "workorder": 2, "jobType": "J", "enteredBy": "HD", "functionalArea": "WEBWEB", "system": "TRACEY", "invoiceText": "Test Job 1", "enteredDate": 20140624, "enteredTime": 900, "defect": "N", "liveUat": "L", "releaseVersion": "F63", "project": "JOBS", "urgent": "Y", "deliverables": [
         {"jobNumber": 123456, "id": 1, "promisedDate": 20140901, "type": "I", "description": "First deliverable", "deliveredDate": 0, "whoDo": "HD", "deleted": "N", "app": "F63", "internal": "Y"},
