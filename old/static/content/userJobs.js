b00d6a0a434d6628082839c237589ba74bea2f63
mishyTheBear <hamish.dickson@gmail.com>
2014-08-09 15:33:34 +0100

    Remove unused modules and old version of app

--- old/static/content/userJobs.js
+++
@@ -1,8 +0,0 @@
-exports.getUserJobs = function(whoDo, status) {
-  var tdata = {};
-  // get the jobs3 data
-  $.getJSON("http://localhost:8070/jobs3/jobtest", function(d) {
-    $.extend(tdata, d);
-    console.log(tdata);
-  });
-}
