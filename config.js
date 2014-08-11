3400f2714698728d651b2e10bf76a231d94cc33b
mishyTheBear <hamish.dickson@gmail.com>
2014-08-11 08:59:04 +0100

    Update ip address

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ exports.JIRA_REST_HOST = "jira.jhc.co.uk";
 // default to live server
 exports.JOBS_REST_HOST = "172.24.24.217";
 
-exports.LEVEL = 'DEV';
+exports.LEVEL = 'TEST';
 
 /*
  * overrides level
