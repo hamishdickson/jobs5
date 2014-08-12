486c8fa173352b3ab1d85ce171b99d939b4ff9c7
mishyTheBear <hamish.dickson@gmail.com>
2014-08-12 08:59:38 +0100

    Add importance filter

--- public/controllers/main.js
+++ public/controllers/main.js
@@ -7,7 +7,10 @@
 
     app.controller('MainController', ['$scope', 'Job', function ($scope, Job) {
 
+        // todo move All out of here and have it's on button to clear out queries
         $scope.statuss = ['All', 'A', 'B', 'H', 'W'];
+
+        $scope.importances = ['1', '2', '3', '4', '5'];
 
         $scope.headingTitle = 'All users jobs';
 
@@ -16,12 +19,24 @@
         $scope.filterByStatus = function (status) {
             if (status == 'All') {
                 $scope.sublist = '';
+                $scope.subImport = '';
                 $scope.headingTitle = 'All users jobs';
             } else {
                 $scope.sublist = status;
                 $scope.headingTitle = 'Only users jobs with status ' + status;
             }
 
-        }
+        };
+
+        $scope.filterByImportance = function (importance) {
+            if (importance == 'All') {
+                $scope.subImport = '';
+                $scope.headingTitle = 'All users jobs';
+            } else {
+                $scope.subImport = importance;
+                $scope.headingTitle = 'Only users jobs with importance ' + importance;
+            }
+            //$scope.subImport = '3';
+        };
     }]);
 })();
